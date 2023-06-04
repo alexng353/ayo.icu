384d09c08f2f8be0e4ee5e13df036ff6b2e30630
Alexander Ng <alexng353@gmail.com>
2023-06-04 13:21:52 -0700

    fix: footer no longer broken

--- components/share/footer.tsx
+++ components/share/footer.tsx
@@ -3,7 +3,7 @@ import styles from "../../styles/bar.module.css";
 export default function Footer() {
   return (
     <div
-      style={{ position: "absolute", bottom: 0, width: "100%" }}
+      style={{ position: "fixed", bottom: 0, width: "100%" }}
       className={styles.footer}
     >
       <div className="container mx-auto pt-4 pb-4 text-center text-gray-400">
